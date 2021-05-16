58e0d5dafc07068c9b0be4dfe639980834245586
Ian F <ian.ferg36@gmail.com>
2021-05-16 15:34:15 -0600

    added more foreign key references to schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS employees_db;
+
 CREATE DATABASE employees_db;
 
 USE employees_db;
@@ -5,20 +7,25 @@ USE employees_db;
 CREATE TABLE department(
     id INTEGER AUTO_INCREMENT NOT NULL,
      name VARCHAR(30),
-     PRIMARY KEY(ID)
+     PRIMARY KEY(id)
 );
 
 CREATE TABLE _role (
+    id INTEGER AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     salary DECIMAL (10, 4),
     department_id INT,
-    PRIMARY KEY(ID)
+    PRIMARY KEY(id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     _role_id INT,
     manager_id INT,
-    PRIMARY KEY(ID)
+    PRIMARY KEY(id),
+    FOREIGN KEY (_role_id) REFERENCES _role(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
